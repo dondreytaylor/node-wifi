b85aea4481905117d9cb1263efde0ec39ccd8c58
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-08-16 14:17:10 -0400

    Testing

--- scan.js
+++ scan.js
@@ -4,5 +4,5 @@ wpa_cli.scan('wlan0', function(err, data){
     wpa_cli.scan_results('wlan0', function(err, data) {
        // returns the results of the BSS scan once it completes
        console.dir(data);
-    }
+    })
 });
